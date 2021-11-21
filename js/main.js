525d0938bbd1ffb2ea0f530fd88581f0f573fd1d
happiness <makenahappiness97@gmail.com>
2021-11-21 19:08:07 +0300

    added funtion

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
-$(document).ready(function() {
-    $('img1').click(function() {
-        $('.design').toggle();
+$(document).ready(function(){
+    $(".des").click(function(){
+        $(".des").hide();
+        $(".design").toggle();
+    });
+    $(".design").click(function(){
+        $(".des").show();
+        $(".design").hide();
     });
 });
 
-$(document).ready(function() {
-    $("img2").click(function() {
+$(document).ready(function(){
+    $(".devel").click(function(){
+        $(".devel").hide();
         $(".development").toggle();
     });
-});
-
-$(document).ready(function() {
-    $("img3").click(function() {
-        $(".product").toggle();
+    $(".development").click(function(){
+        $(".devel").show();
+        $(".development").hide();
     });
 });
+
+$(document).ready(function(){
+    $(".management").click(function(){
+        $(".management").hide();
+        $(".product").toggle();
+    });
+    $(".product").click(function(){
+        $(".management").show();
+        $(".product").hide();
+    });
+});
+
